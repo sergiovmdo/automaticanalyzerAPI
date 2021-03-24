678be758f63c6dfaafd57d9006e370b2eacd4a8f
sergiovmdo97 <sergiovazquezmontesdeoca@gmail.com>
2021-03-24 19:32:57 +0100

    Changed return type of create user

--- src/main/java/com/aaa/automaticanalyzer/api/user/rest/RegisterController.java
+++ src/main/java/com/aaa/automaticanalyzer/api/user/rest/RegisterController.java
@@ -2,6 +2,10 @@ package com.aaa.automaticanalyzer.api.user.rest;
 
 import com.aaa.automaticanalyzer.api.user.domain.UserRestInput;
 import com.aaa.automaticanalyzer.api.user.business.UserService;
+import com.aaa.automaticanalyzer.model.User;
+import jdk.nashorn.internal.objects.annotations.Constructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,24 +23,14 @@ public class RegisterController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> createUser(@RequestBody final UserRestInput userRestInput) {
-//        try
-//        {
-//            return PockMappers.mapPockToPockRestOutput(pocksService.createPock(pockRestInput, user));
-//        }
-//        catch (CategoryNotValid categoryNotValid)
-//        {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("Category %d is not valid", pockRestInput.getCategory()));
-//        }
-//        catch (PockNotFound pockNotFound)
-//        {
-//            // Let's just treat this as an internal server error as it should never enter here
-//            // A log in case we enter
-//            log.error("Returning a freshly created pock [{}] returned PockNotFound", pockNotFound.getId());
-//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-        userService.createUser(userRestInput);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<TokenResponse> createUser(@RequestBody final UserRestInput userRestInput) {
+        User user = userService.createUser(userRestInput);
+        return ResponseEntity.ok(new TokenResponse(user.getToken()));
     }
 
+    @Data
+    @AllArgsConstructor
+    private class TokenResponse {
+        private final String token;
+    }
 }
